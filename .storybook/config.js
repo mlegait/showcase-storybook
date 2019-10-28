ef82df8c8fa5204cf521c89c8c8143a19235bcb9
Michele Legait <mlegait@pros.com>
2019-10-28 22:01:50 +0100

    Install addon-a11y

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,13 @@
-import { withKnobs } from "@storybook/addon-knobs";
-import { addDecorator, configure } from "@storybook/react";
-import React from "react";
-import Container from "./Container";
+import { withA11y } from '@storybook/addon-a11y';
+import { withKnobs } from '@storybook/addon-knobs';
+import { addDecorator, configure } from '@storybook/react';
+import React from 'react';
+import Container from './Container';
 
 addDecorator(story => <Container story={story} />);
 
 addDecorator(withKnobs);
+addDecorator(withA11y);
 
 // automatically import all files ending in *.stories.js
-configure(require.context("../src", true, /\.stories\.js$/), module);
+configure(require.context('../src', true, /\.stories\.js$/), module);
